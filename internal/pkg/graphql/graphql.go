46803bfef899abd9576db60d4acce08ba9aca4e6
Timothy White <timma.j.white@gmail.com>
2019-08-31 23:12:18 -0600

    added gql struct

---
+++ internal/pkg/graphql/graphql.go
@@ -0,0 +1,56 @@
+package graphql
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/graph-gophers/graphql-go"
+)
+
+// GraphQl graphql handler
+type GraphQl struct {
+	Schema *graphql.Schema
+}
+
+// BuildSchema builds schema
+func (g *GraphQl) BuildSchema(schema string, resolver interface{}) {
+	g.Schema = graphql.MustParseSchema(schema, resolver)
+}
+
+var (
+	// ErrQueryNameNotProvided is thrown when a name is not provided
+	ErrQueryNameNotProvided = errors.New("no query was provided in the HTTP body")
+)
+
+// LambdaHandler is the Lambda function handler
+func (g *GraphQl) LambdaHandler(context context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	log.Printf("Processing Lambda request %s\n", request.RequestContext.RequestID)
+	// If no query is provided in the HTTP request body, throw an error
+	if len(request.Body) < 1 {
+		return events.APIGatewayProxyResponse{}, ErrQueryNameNotProvided
+	}
+
+	var params struct {
+		Query         string                 `json:"query"`
+		OperationName string                 `json:"operationName"`
+		Variables     map[string]interface{} `json:"variables"`
+	}
+
+	if err := json.Unmarshal([]byte(request.Body), &params); err != nil {
+		log.Print("Could not decode body", err)
+	}
+
+	response := g.Schema.Exec(context, params.Query, params.OperationName, params.Variables)
+	responseJSON, err := json.Marshal(response)
+	if err != nil {
+		log.Print("Could not decode body")
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       string(responseJSON),
+		StatusCode: 200,
+	}, nil
+}
